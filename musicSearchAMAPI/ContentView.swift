62298bab7d967960f09f2f911829523260d9e5d2
ioszm <zmpato@yahoo.com>
2021-02-22 14:09:06 -0400

    Initial Commit

---
+++ musicSearchAMAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  musicSearchAMAPI
+//
+//  Created by Zach mills on 2/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ musicSearchAMAPI/musicSearchAMAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicSearchAMAPIApp.swift
+//  musicSearchAMAPI
+//
+//  Created by Zach mills on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct musicSearchAMAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
